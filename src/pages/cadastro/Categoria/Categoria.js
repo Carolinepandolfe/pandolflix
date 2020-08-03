d37d8dbb430f736f2c60e2f00ad6a341b34fdd2a
caroline pandolfe <carolinepandolfe@yahoo.com.br>
2020-08-03 18:49:43 -0300

    Fix local url request

--- src/pages/cadastro/Categoria/Categoria.js
+++ src/pages/cadastro/Categoria/Categoria.js
@@ -31,7 +31,7 @@ function CadastroCategoria() {
 
   useEffect(() => {
     const URL = window.location.hostname.includes('localhost')
-      ? 'http://localhost:8080'
+      ? 'http://localhost:8080/categorias'
       : 'https://pandolflix.herokuapp.com/categorias';
     fetch(URL)
       .then(async (res) => {
